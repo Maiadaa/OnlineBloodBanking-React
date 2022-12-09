44c9f3f61472fc08a50847ce04b92c742ecb61ba
Maiadaa <maiadahkhaled@gmail.com>
2022-12-09 13:16:45 +0200

    updated app.js

--- src/App.js
+++ src/App.js
@@ -11,61 +11,83 @@ import AcceptPatientRequests from "./pages/AcceptRequestPage";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<h1>Home Page</h1>}/>
-        <Route path="/getAllPatients" element={
-          <div className="flex justify-center">
-          <PatientsPage />
-        </div>
-        }/>
-        <Route path="/AddNewPatient" element={
-          <div className="flex justify-center bg-gray-50 ">
-          <AddPatientPage />
-        </div>
-        }/>
-        <Route path="/hospitals" element={
-          <div className="flex justify-center">
-          <HospitalsPage />
-        </div>
-        }/>
-        <Route path="/patients/makeRequest" element={
-          <div className="flex justify-center">
-          <MakeRequestPage />
-        </div>
-        }/>
-         <Route path="LabAdminMenu" element={
-          <div className="flex justify-center">
-          <LabAdminHomePage />
-        </div>
-        }/>
-        <Route path="LabManagerMenu" element={
-          <div className="flex justify-center">
-          <LabManagrHomePage />
-        </div>
-        }/>
-        <Route path="InsertBloodBag" element={
-          <div className="flex justify-center">
-          <LabManagrHomePage />
-        </div>
-        }/>
-        <Route path="/patients/viewReuest/:patientId" element={
-          <div className="flex justify-center">
-          <ViewPatientRequests />
-        </div>
-        }/>
-        <Route path="/patients/ModifyRequest/:patientId/:requestId" element={
-          <div className="flex justify-center">
-          <ModifyRequestPage />
-        </div>
-        }/>
-        <Route path="/patients/AcceptRequest/:patientId/:requestId" element={
-          <div className="flex justify-center">
-          <AcceptPatientRequests />
-        </div>
-        }/>
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<h1>Home Page</h1>} />
+            <Route path="/hospitals" element={
+              <div className="flex justify-center">
+                <HospitalsPage />
+              </div>
+            } />
+            <Route path="/hospitals/report" element={
+              <div className="flex justify-center">
+                <HospitalsYearlyReportPage />
+              </div>
+            } />
+            <Route path="/hospitals/report/:hospitalId" element={
+              <div className="flex justify-center">
+                <HospitalReportPage />
+              </div>
+            } />
+            <Route path="/hospitals/addHospital" element={
+              <div className="flex justify-center">
+                <AddHospitalPage />
+              </div>
+            } />
+            <Route path="/getAllPatients" element={
+              <div className="flex justify-center">
+                <PatientsPage />
+              </div>
+            } />
+            <Route path="/AddNewPatient" element={
+              <div className="flex justify-center bg-gray-50 ">
+                <AddPatientPage />
+              </div>
+            } />
+            <Route path="/patients/makeRequest" element={
+              <div className="flex justify-center">
+                <MakeRequestPage />
+              </div>
+            } />
+            <Route path="LabAdminMenu" element={
+              <div className="flex justify-center">
+                <LabAdminHomePage />
+              </div>
+            } />
+            <Route path="LabManagerMenu" element={
+              <div className="flex justify-center">
+                <LabManagrHomePage />
+              </div>
+            } />
+            <Route path="InsertBloodBag" element={
+              <div className="flex justify-center">
+                <LabManagrHomePage />
+              </div>
+            } />
+            <Route path="/patients/viewReuest/:patientId" element={
+              <div className="flex justify-center">
+                <ViewPatientRequests />
+              </div>
+            } />
+            <Route path="/patients/ModifyRequest/:patientId/:requestId" element={
+              <div className="flex justify-center">
+                <ModifyRequestPage />
+              </div>
+            } />
+            <Route path="/patients/AcceptRequest/:patientId/:requestId" element={
+              <div className="flex justify-center">
+                <AcceptPatientRequests />
+              </div>
+            } />
+            <Route path="/signup" element={<SignupPage />} />
+            <Route path="/signin" element={<SigninPage />} />
+
+          </Routes>
+        </Layout>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
