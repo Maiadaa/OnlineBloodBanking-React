30e4928ec752626ec0bc5378394ba76d9d763e20
Maiadaa <maiadahkhaled@gmail.com>
2022-12-11 23:35:53 +0200

    modify acc links

--- src/UI/navbar/Navbar.js
+++ src/UI/navbar/Navbar.js
@@ -4,17 +4,23 @@ import NavItem from './NavItem';
 
 const Navbar = () => {
   var homeURL;
+  var accountURL;
   const authContext = useContext(AuthContext);
   if(authContext.role == "Lab Manager"){
     homeURL = "/LabManagerMenu";
+    accountURL = "/LabManager/Update";
   }else if(authContext.role == "Lab Admin"){
     homeURL = "/LabAdminMenu";
+    accountURL = "/LabAdmin/Update";
   }else if(authContext.role == "Super Admin"){
     homeURL = "/SuperAdminHomePage";
+    accountURL = "/superAdmin/Update";
   }else if (authContext.role == "Doctor"){
     //homeURL = "/DoctorHomePage";
+    //accountURL = "/";
   }else if (authContext.role == "Donor"){
-    //homeURL = "/DonorHomePage";
+    homeURL = "/DonorHomePage";
+    //accountURL = "";
   }
 
   return (
@@ -24,7 +30,7 @@ const Navbar = () => {
         <NavItem to="/">About</NavItem>
         {!authContext.token && <NavItem to="/signin">Sign In</NavItem>}
         {!authContext.token && <NavItem to="/signup">Sign Up</NavItem>}
-        {authContext.token && <NavItem to="/modifyAccount">Account</NavItem>}
+        {authContext.token && <NavItem to={homeURL}>Account</NavItem>}
         {authContext.token && <NavItem to="/logout">Log out</NavItem>}
       </ul>
     </nav>
